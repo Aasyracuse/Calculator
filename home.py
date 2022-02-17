acb1246391313eb4d3d1682161c7d13ad97f3bac
Aasyracuse <85194268+Aasyracuse@users.noreply.github.com>
2022-02-16 23:08:31 -0500

    I did it

--- home.py
+++ home.py
@@ -9,13 +9,21 @@ def divide(x,y):
 
 print("(1.Add/2.Subtract/3.Multiply/4.Divide)")
 
-choice= input("Choose a operation (1,2,3,4)")
+
 while True:  
+    choice= input("Choose a operation (1,2,3,4): ")
     if choice in ('1','2','3','4'):
-        num1= input("Enter your first number: ")
-        num2= input("Enter your second number: ")
+        num1= float(input("Enter your first number: "))
+        num2= float(input("Enter your second number: "))
     if choice == '1':
-        add(num1 + num2)
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+         print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+         print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+         print(num1, "/", num2, "=", divide(num1, num2))
+
 
     
 else:
